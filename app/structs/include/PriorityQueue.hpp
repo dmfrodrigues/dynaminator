22ed8c6276c5390fc0f8a88109ec8d0e574f4a4b
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2023-03-04 20:00:23 +0100

    Binary heap

---
+++ app/structs/include/PriorityQueue.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+template<class T>
+class PriorityQueue {
+public:
+    class Element {
+    public:
+        virtual T getValue() = 0;
+        virtual void decreaseKey(T t) = 0;
+    };
+
+    virtual T top() = 0;
+    virtual size_t size() = 0;
+    virtual Element& push(T t) = 0;
+    virtual T pop() = 0;
+};
