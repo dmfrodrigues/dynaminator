3347ea771fc7ef3747013c5f0b43b6c5bc9a3957
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2023-04-09 19:45:56 +0100

    Implemented QuadraticGuessSolver

---
+++ app/include/opt/QuadraticGuessSolver.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "opt/SolverWithInitialSolutions.hpp"
+#include "opt/UnivariateSolver.hpp"
+
+/**
+ * @brief Solver that guesses the value of the next solution.
+ *
+ * Provides three solutions to the solver. Solutions are calculated as follows:
+ * $s_{n+1} = (1-\alpha) s_n + \alpha x_n$
+ * $s_1 = M * s_n$
+ * $s_2 = s_1 * 10^{-m}$
+ * $s_3 = s_1 * 10^{m}$
+ * where:
+ * - $s_n$ are solution estimates.
+ * - $\alpha$ is the exponential smoothing factor; should be a small value,
+ *   typically 0.1 or 0.2 to prevent excessive jitter.
+ * - $M$ is the multiplicative adjustment factor. This is because a small
+ *   $\alpha$ avoids jitter, but this delays adapting to more recent answers.
+ *   Since we already know where the process is going (e.g., we have an idea
+ *   that the real value is 85% of the estimate), we can adjust the estimation
+ *   by multiplying all solutions with $M$.
+ * - $m$ is the margin.
+ */
+class QuadraticGuessSolver: public UnivariateSolver {
+    SolverWithInitialSolutions &solver;
+    Var s;
+    Var alpha;
+    Var multAdjust;
+    Var margin;
+
+   public:
+    /**
+     * @brief Construct a new Quadratic Solver Guesser object.
+     *
+     * @param initialSolution   Initial solution
+     * @param alpha             Exponential smoothing factor
+     * @param multAdjust        Multiplicative adjustment factor
+     * @param margin            Margin (in logarithmic scale, log10)
+     */
+    QuadraticGuessSolver(
+        SolverWithInitialSolutions &solver,
+        Var initialSolution,
+        Var alpha = 0.2,
+        Var multAdjust = 1.0,
+        Var margin = 1.0
+    );
+    
+    virtual Var solve(Problem p);
+};
---
+++ app/src/opt/QuadraticGuessSolver.cpp
@@ -0,0 +1,29 @@
+#include "opt/QuadraticGuessSolver.hpp"
+
+#include <cmath>
+
+typedef UnivariateSolver::Var Var;
+
+QuadraticGuessSolver::QuadraticGuessSolver(
+    SolverWithInitialSolutions &solver_,
+    Var initialSolution_,
+    Var alpha_,
+    Var multAdjust_,
+    Var margin_
+):
+    solver(solver_),
+    s(initialSolution_),
+    alpha(alpha_),
+    multAdjust(multAdjust_),
+    margin(margin_) {}
+
+Var QuadraticGuessSolver::solve(Problem prob) {
+    Var p = pow(10, margin);
+    Var s1 = s * multAdjust;
+    Var s2 = s * p;
+    Var s3 = s / p;
+    solver.addInitialSolutions(s1, s2, s3);
+    Var x = solver.solve(prob);
+    s = (1.0 - alpha) * s + alpha * x;
+    return x;
+}
