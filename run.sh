876ba15e0c4ede9c4681c0d070de548a553b385b
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2023-04-14 13:56:53 +0100

    Fixed production image

--- run.sh
+++ run.sh
@@ -3,7 +3,4 @@
 set -e
 
 cd /data
-dynaminator 2>&1 | sed "s/^/[simulator] /" &
-
-tail -f /var/log/apache2/error.log | sed "s/^/[apache:error] /" &
-/usr/sbin/apache2ctl -DFOREGROUND | sed "s/^/[apache:acces] /"
+dynaminator
